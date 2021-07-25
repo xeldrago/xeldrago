e2bfb4d6dcbffb559b37b88c1afc26306f13d856
Jeff Peter <xeldrago>
2021-07-26 03:23:56 +0530

    changes on site and py

--- python mini works/phone number details finder.py
+++ python mini works/phone number details finder.py
@@ -1,7 +1,9 @@
 #Phone number details py
 import phonenumbers
 from phonenumbers import timezone, geocoder, carrier
-phoneNumber = phonenumbers.parse( "+919994198795")
+number1=input("type in the number for which u want the info \n")
+number2="+91"+number1
+phoneNumber = phonenumbers.parse(number2)
 timeZone = timezone.time_zones_for_number(phoneNumber)
 Carrier = carrier.name_for_number(phoneNumber, 'en')
 Region=geocoder.description_for_number(phoneNumber, 'en')
