7d08ec855150b929235952dbcf6cdd2b3ceede7a
Jeff Peter <xeldrago>
2021-07-26 03:23:56 +0530

    changes on site and py

---
+++ python mini works/oneliners/oneliners.py
@@ -0,0 +1,12 @@
+#some oneline programs that are funny lol
+#palindrome 
+sentence= "radar"
+se=sentence.find(sentence[::-1]) 
+if se == 0:
+    print("its a palindrome")
+else:
+    print("nope its not a pali,pal")
+
+#ifelse 
+
+
