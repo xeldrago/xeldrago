2e544e9ad9fca133e2dc8b9f3d2980ad97b6210f
Jeff Peter <xeldrago>
2021-07-28 21:52:13 +0530

    umm

---
+++ simple python works/classes.py
@@ -0,0 +1,13 @@
+m1=int(input("enter three numbers, ill provide ya the sum!!!\nstarting with the first number="))
+
+m2=int(input("second="))
+m3=int(input("third="))
+
+
+def verdict():
+    total=m1+m2+m3
+    print(total)
+i=2
+for i in range(1,30):
+    verdict()
+
