ca46cc9dfb718f4ea844c0b75bc969103d0b5f76
Jeff Peter <xeldrago>
2021-08-05 15:57:05 +0530

    python server improvised

--- python mini works/python server/chrome_open_test.py
+++ python mini works/python server/chrome_open_test.py
@@ -1,7 +1,6 @@
 import webbrowser
 
-url = 'https://pythonexamples.org'
-webbrowser.register('chrome',
-	None,
-	webbrowser.BackgroundBrowser("C://Program Files (x86)//Google//Chrome//Application//chrome.exe"))
-webbrowser.get('chrome').open(url)
+url = 'https://google.com')
+
+Chrome="C://Program Files (x86)//Google//Chrome//Application//chrome.exe"
+webbrowser.get(Chrome).open(url)
