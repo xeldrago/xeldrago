1a2afcdf8954237211a7fb2fd0a86341549f70c9
Jeff Peter <xeldrago>
2021-10-09 11:58:59 -0700

    tried a jiggler mysel

---
+++ python mini works/MouseKeyboardAuto.py
@@ -0,0 +1,14 @@
+import mouse
+import keyboard
+#KR=keyboard.record(until='esc')
+#play=keyboard.play(KR, speed_factor=2)
+records=mouse.record()
+
+if 
+while mouse.click('right'):
+    
+    #mouse.click('left')
+    mouse.play(records[:-1], speed_factor=1.0, include_clicks=True, include_moves=True, include_wheel=True)#press right click to start playing after u do ur jiggle or work
+      
+
+
