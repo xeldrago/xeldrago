36f25454c6a292cdc614da630eccb24007eb6a31
xeldrago <jeffrynspr@gmail.com>
2022-01-29 22:11:49 +0530

    spelling correction

--- simple python works/jmaker.py
+++ simple python works/jmaker.py
@@ -1,6 +1,9 @@
 import random
+import pipreqs
+pipreqs.process_file("jmaker.py")
 print("hi its jeno")
 pp=['so ok i love u too','nice!!','i love u too','so swweet jeff']
+
 while i!=14:
  i+=1
 
@@ -9,22 +12,22 @@ while i!=14:
   ty=random.choices(pp)
   if ty == pp[0]:
    e=input("but u have lied a lot")
-  if e!=0:
-   print("ok then ... as u say... love u!")
-  else:
-   print("how sweet u")
-  else ty == pp[1]:
+   if e!=0:
+       print("ok then ... as u say... love u!")
+   else:
+       print("how sweet u")
+  elif ty == pp[1]:
    r=input("u are perhaps nicer if u apologize right or become thin like me cos i hate ur face man")
-  if r!=1:
-   print("mmm... try hader")
-  else:
-   print("right again")
-  else:
-   se=input("whatvr u say jeff... jus be good")
+   if r!=1:
+    print("mmm... try hader")
+   else:
+    print("right again")
+  
+  se=input("whatvr u say jeff... jus be good")
   if se!=4:
    print("so umm.. ok love u")
   else:
    print("mmm")
  else:
-	print("not even a 'hi' mm.. very bad")										
+   print("not even a 'hi' mm.. very bad")										
 	
