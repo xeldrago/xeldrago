b22e81f0ab9f32a3872662335dce55f1f9c3ced1
xeldrago <jeffrynspr@gmail.com>
2022-01-30 19:51:06 +0530

    trash removal

--- python mini works/python server/dist/PythonServer.bat
+++
@@ -1,4 +0,0 @@
-echo "your py server will launch and deploy from your current working directory, brace up!!"
-cd /d "G:\git and repos\vroom-vroom\python mini works\python server\dist"
-PythonServer.exe
-
--- python mini works/wifi password finder (only connected networks)/wifi password finder.py
+++
@@ -1,4 +0,0 @@
-import os
-net_id=input("type the network name connected to your device\n")
-os.system(f"netsh wlan show profile {net_id} key=clear")
-print("now from the output, look for key content, that is your lost or forgotten password")
